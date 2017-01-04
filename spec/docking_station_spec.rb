03608027faf61ce12b5525357a7c912181468228
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-04 17:47:24 +0000

    write and pass test for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,13 @@
 require "docking_station.rb"
 
 describe DockingStation do
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
+  end
 end
+
+=begin (one-liner syntax; also passes test)
+describe "responds to release_bike" do
+  it { should respond_to :release_bike }
+end
+=end
